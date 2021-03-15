ee402abc8a44622964c92a032d2b5827b20773e6
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-03-15 11:13:03 +0100

    Add acccelerometer, kotlinize

--- app/src/main/java/com/gluszczykk/qarantanna/MainActivity.kt
+++ app/src/main/java/com/gluszczykk/qarantanna/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.gluszczykk.qarantanna
 
 import android.hardware.Sensor
+import android.hardware.Sensor.TYPE_ACCELEROMETER
 import android.hardware.Sensor.TYPE_PROXIMITY
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -11,26 +12,44 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    fun SensorEvent.toLabel() = when (sensor.type) {
+        TYPE_PROXIMITY -> "${sensor.name} ${values[0]} cm"
+        TYPE_ACCELEROMETER -> "${sensor.name} x:${values[0]} y:${values[1]},z ${values[2]}"
+        else -> ""
+    }
+
+    private val sensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            findViewById<TextView>(R.id.sensor_output).text = event.toLabel()
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setUpProximitySensor()
+        val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        setUpProximitySensor(sensorManager)
+        setUpAccelerometerSensor(sensorManager)
     }
 
-    private fun setUpProximitySensor() {
-        val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+    private fun setUpProximitySensor(sensorManager: SensorManager) {
         val proximitySensor = sensorManager.getDefaultSensor(TYPE_PROXIMITY)
         sensorManager.registerListener(
-            object : SensorEventListener {
-                override fun onSensorChanged(event: SensorEvent) {
-                    findViewById<TextView>(R.id.sensor_output).text = "${event.sensor.name} ${event.values[0]} cm"
-                }
-
-                override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
-                }
-
-            }, proximitySensor,
+            sensorEventListener, proximitySensor,
             SensorManager.SENSOR_DELAY_UI
         )
     }
+
+    private fun setUpAccelerometerSensor(sensorManager: SensorManager) {
+        val proximitySensor = sensorManager.getDefaultSensor(TYPE_ACCELEROMETER)
+        sensorManager.registerListener(
+            sensorEventListener, proximitySensor,
+            SensorManager.SENSOR_DELAY_UI
+        )
+    }
+
 }
