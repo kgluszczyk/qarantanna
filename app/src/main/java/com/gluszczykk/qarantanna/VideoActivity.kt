76c98c0654b4008785655717cb22aad8620cc0d7
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-03-15 15:18:50 +0100

    Add video activity

--- app/src/main/java/com/gluszczykk/qarantanna/VideoActivity.kt
+++ app/src/main/java/com/gluszczykk/qarantanna/VideoActivity.kt
@@ -1,6 +1,8 @@
 package com.gluszczykk.qarantanna
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
 class VideoActivity : AppCompatActivity() {
@@ -8,5 +10,9 @@ class VideoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
+        findViewById<VideoView>(R.id.video).run {
+            setVideoURI(Uri.parse("android.resource://$packageName/${R.raw.video}"))
+            start()
+        }
     }
 }
