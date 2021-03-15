079efa352d9c22246764bc5495a2c289b9e7cfdd
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-03-15 10:47:12 +0100

    Proximity sensor

--- app/src/main/java/com/gluszczykk/qarantanna/MainActivity.kt
+++ app/src/main/java/com/gluszczykk/qarantanna/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.gluszczykk.qarantanna
 
+import android.hardware.Sensor
+import android.hardware.Sensor.TYPE_PROXIMITY
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpProximitySensor()
+    }
+
+    private fun setUpProximitySensor() {
+        val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        val proximitySensor = sensorManager.getDefaultSensor(TYPE_PROXIMITY)
+        sensorManager.registerListener(
+            object : SensorEventListener {
+                override fun onSensorChanged(event: SensorEvent) {
+                    findViewById<TextView>(R.id.sensor_output).text = "${event.sensor.name} ${event.values[0]} cm"
+                }
+
+                override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+                }
+
+            }, proximitySensor,
+            SensorManager.SENSOR_DELAY_UI
+        )
     }
 }
