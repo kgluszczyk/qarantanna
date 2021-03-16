b4984bf6441977ffb3297b6c2b75ecfa5dfe5cf4
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-03-16 16:18:02 +0100

    Add intrumental tests

--- app/src/androidTest/java/com/gluszczykk/qarantanna/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/gluszczykk/qarantanna/ExampleInstrumentedTest.kt
@@ -1,9 +1,15 @@
 package com.gluszczykk.qarantanna
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.*
 
-import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
@@ -16,10 +22,13 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
 
+    @get:Rule
+    var rule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.gluszczykk.qarantanna", appContext.packageName)
+        onView(withId(R.id.sensor_output))
+            .check(matches(isDisplayed()))
+            .check(matches(withText("Sensory tu bedą")))
     }
 }
